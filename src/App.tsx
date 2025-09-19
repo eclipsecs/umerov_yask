7b28a37b56fb042a6b330a1d25683caf37da3056
Umerov Javokhir <1xedera@gmail.com>
2025-09-20 00:56:48 +0500

    THE PART I NEED THE MOST

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,54 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+import { Toaster as Sonner } from "sonner";
+import { TooltipProvider } from "./components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Layout from "./components/Layout";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import ArticlesPage from "./pages/ArticlesPage";
+import ArticleDetailPage from "./pages/ArticleDetailPage";
+import TeachingsPage from "./pages/TeachingsPage";
+import JournalsPage from "./pages/ReadingPage";
+import ReadingPageDetail from "./pages/ReadingPageDetail";
+import IELTSEssaysPage from "./pages/IELTSEssaysPage";
+import EssayDetailPage from "./pages/IELTSEssayDetailPage";
+import BooksPage from "./pages/books/BooksPage";
+import BookDetailPage from "./pages/books/BookDetailPage";
+import NotFound from "./pages/NotFound";
 
-const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+const queryClient = new QueryClient();
+
+const App: React.FC = () => {
   return (
-    <div className="min-h-screen">
-      {children}
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <BrowserRouter>
+            <Layout>
+              <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/articles" element={<ArticlesPage />} />
+                <Route path="/article/:id" element={<ArticleDetailPage />} />
+                <Route path="/career" element={<TeachingsPage />} />
+                <Route path="/journals" element={<JournalsPage />} />
+                <Route path="/journals/:id" element={<ReadingPageDetail />} />
+                <Route path="/ielts-essays" element={<IELTSEssaysPage />} />
+                <Route path="/ielts-essays/:id" element={<EssayDetailPage />} />
+                <Route path="/books" element={<BooksPage />} />
+                <Route path="/books/:id" element={<BookDetailPage />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </Layout>
+          </BrowserRouter>
+        </TooltipProvider>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 };
 
-export default Layout;
+export default App;
